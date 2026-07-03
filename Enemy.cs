ce896bd39d2aeb1b660d375207592dc7e6eb02b9
moorthimaheshwari007-arch <moorthimaheshwari007@gmail.com>
2026-07-03 20:25:35 +0530

    Crunchy Battle

---
+++ Enemy.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ENEMY : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 2f;
+    public int health = 100;
+
+    void Update()
+    {
+        transform.LookAt(player);
+        transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        health -= dmg;
+        if (health <= 0) Destroy(gameObject);
+    }
+}
