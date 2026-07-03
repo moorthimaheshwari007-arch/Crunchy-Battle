e1c5da08df6574e383e1bd584669d45680c52145
moorthimaheshwari007-arch <moorthimaheshwari007@gmail.com>
2026-07-03 20:17:03 +0530

    Crunchy Battle

---
+++ Bullet.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BULLET : MonoBehaviour
+{
+    public float speed = 25f;
+    public int damage = 20;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<ENEMYHEALTH>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
