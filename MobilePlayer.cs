686c41e765a37a1e324596d63394202b87e863dc
moorthimaheshwari007-arch <moorthimaheshwari007@gmail.com>
2026-07-03 20:44:07 +0530

    Crunchy Battle

--- MobilePlayer.cs
+++ MobilePlayer.cs
@@ -1,34 +1,8 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
-    public CharacterController controller;
-    public FloatingJoystick joystick;
-
-    public float speed = 5f;
-
     public int health = 100;
-
-    public bool falling;
-
-    void Update()
-    {
-        Vector3 move =
-            transform.right * joystick.Horizontal +
-            transform.forward * joystick.Vertical;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        if (falling)
-        {
-            controller.Move(Vector3.down * 6f * Time.deltaTime);
-        }
-    }
-
-    public void StartFall()
-    {
-        falling = true;
-    }
 
     public void TakeDamage(int dmg)
     {
@@ -36,7 +10,7 @@ public class PlayerController : MonoBehaviour
 
         if (health <= 0)
         {
-            Debug.Log("PLAYER DEAD");
+            Debug.Log("GAME OVER");
         }
     }
 }
