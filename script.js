1e2d780e99657b19d756097c39ae91c491fb1dbe
moorthimaheshwari007-arch <moorthimaheshwari007@gmail.com>
2026-07-03 18:51:32 +0530

    Crunchy Battle
    Initial implementation of player object and game functions.

---
+++ script.js
@@ -0,0 +1,41 @@
+// Crunchy Battle
+
+const player = {
+    id: "CB" + Math.floor(Math.random() * 1000000),
+    name: "Guest",
+    coins: 999999,
+    gems: 999999,
+    level: 1
+};
+
+console.log("Welcome to Crunchy Battle!");
+console.log("Player ID:", player.id);
+
+function showHelp() {
+    alert(
+`HOW TO PLAY
+
+1. Press PLAY
+2. Join a match
+3. Jump from the moving train
+4. Find weapons
+5. Defeat enemies
+6. Be the last survivor`
+    );
+}
+
+function playGame() {
+    alert("Crunchy Battle is under development.");
+}
+
+function showProfile() {
+    alert(
+`PLAYER PROFILE
+
+ID: ${player.id}
+Name: ${player.name}
+Level: ${player.level}
+Coins: ${player.coins}
+Gems: ${player.gems}`
+    );
+}
